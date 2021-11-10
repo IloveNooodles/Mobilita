2cd86e732c1e1ef629f2aae43aef92bd5b81c236
arikrayi <83354451+arikrayi@users.noreply.github.com>
2021-11-10 17:42:56 +0700

    edit ability.h

--- lib/ability/ability.h
+++ lib/ability/ability.h
@@ -8,7 +8,6 @@
 
 typedef struct {
   int time; //ngitung dari 10 sampe 1 atau dari 1 smpe 10 nanti baru set freezenya off
-  int bonusCapacity; //ini jumlah banyaknya bonus item
   boolean freezeTime; //check lagi berenti waktu apa engga
   boolean heavyItem; //check ada heavy apa engga 
 } Ability;
@@ -17,14 +16,13 @@ typedef struct {
 //Selektor
 #define FREEZE(l) (l).freezeTime
 #define TIME(l) (l).time
-#define BONUSCAPACITY(l) (l).bonusCapacity
 #define HEAVYITEM(l) (l).heavyItem
 
 void createAbility(Ability b);
 void checkHeavyIteminBag(Ability b, Stack s);
 void activateSpeedBoost(Ability b);
 void deactivateSpeedBoost(Ability b);
-void activateIncreaseCapacity(Ability b);
+void activateIncreaseCapacity();
 void activateReturnToSender(Ability b);
 
 #endif
